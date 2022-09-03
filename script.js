d3115892092a61a98f8b1c25f3456df93818f6a1
Bohdan <makohoniuk.bo@gmail.com>
2022-09-03 17:25:39 +0300

    test-1

---
+++ script.js
@@ -0,0 +1,27 @@
+// document.addEventListener(
+//   "scroll",
+//   _.throttle(() => {
+//     console.log("Scroll handler call every 300ms");
+//   }, 300)
+// );
+
+
+const vanillaOutput = document.querySelector(".output.vanilla");
+const throttledOutput = document.querySelector(".output.throttled");
+const eventCounter = {
+  vanilla: 0,
+  throttled: 0
+};
+
+document.addEventListener("scroll", () => {
+  eventCounter.vanilla += 1;
+  vanillaOutput.textContent = eventCounter.vanilla;
+});
+
+document.addEventListener(
+  "scroll",
+  _.throttle(() => {
+    eventCounter.throttled += 1;
+    throttledOutput.textContent = eventCounter.throttled;
+  }, 300)
+);
